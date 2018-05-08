9a4d315b9eaa808740417476031c9b0f36d7c437
pedja77 <pedja77@stcable.net>
2018-05-08 21:25:50 +0200

    Zadatak 6

--- src/pages/AddCar.vue
+++ src/pages/AddCar.vue
@@ -3,15 +3,15 @@
   <form @submit.prevent="submit">
   <div class="form-group" >
     <label for="brand">Brand</label>
-    <input type="text" class="form-control" id="brand"  placeholder="Enter brand" v-model="car.brand">
+    <input type="text" class="form-control" id="brand"  placeholder="Enter brand" v-model="car.brand" minlength="2" required>
   </div>
   <div class="form-group">
     <label for="model">Model</label>
-    <input type="text" class="form-control" id="model" placeholder="Enter model" v-model="car.model">
+    <input type="text" class="form-control" id="model" placeholder="Enter model" v-model="car.model" minlength="2" required>
   </div>
   <div class="form-group">
     <label for="year">Year</label>
-    <select type="text" class="form-control" id="year" v-model="car.year">
+    <select type="text" class="form-control" id="year" v-model="car.year" required>
         <option v-for="year in years" :key="year" :value="year">{{ year }}</option>
     </select>
   </div>
@@ -21,15 +21,15 @@
   </div>
   <div class="form-group">
     <label for="number-of-doors">Number of doors</label>
-    <input type="number" class="form-control" id="number-of-doors" min="0" v-model="car.numberOfDoors">
+    <input type="number" class="form-control" id="number-of-doors" min="0" v-model="car.numberOfDoors" required>
   </div>
   <div class="form-group form-check">
-    <input type="checkbox" class="form-check-input" id="is-automatic" v-model="car.isAutomatic">
+    <input type="checkbox" class="form-check-input" id="is-automatic" v-model="car.isAutomatic" required>
     <label class="form-check-label" for="is-automatic">Is gearbox automatic</label>
   </div>
   <div class="form-group">
     <div class="form-check">
-      <input class="form-check-input" type="radio" name="engine" id="diesel" value="diesel" v-model="car.engine" checked>
+      <input class="form-check-input" type="radio" name="engine" id="diesel" value="diesel" v-model="car.engine" checked="checked">
       <label class="form-check-label" for="diesel">
         Diesel
       </label>
